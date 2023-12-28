3ee5552470f4c21f70930252714ad79625b13039
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2023-12-28 21:25:30 +0900

    Done this prod

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ const detailsEl = document.querySelectorAll('.accordion');
 
 detailsEl.forEach((el) => {
   el.addEventListener('toggle', (e) => {
-    console.log(e.target.open);
     const imgEl = el.children[0].children[1];
-    console.log(imgEl);
-
     if (e.target.open) {
       imgEl.src = '/assets/images/icon-minus.svg';
     } else {
